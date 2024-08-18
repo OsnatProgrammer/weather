5716e107aa2e016a94082c83ca0cd49632eb4509
osnat <osnat00972@gmail.com>
2024-08-18 15:31:07 +0300

    add disconnection

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -5,12 +5,19 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
 
+    const disconnection = () => {
+        localStorage.removeItem('user');
+    }
+
     return (
         <div className={styles.header}>
             <ul className={styles.menu}>
                 <li><Link to="/">ראשי</Link></li>
                 <li><Link to="/mador">מדור</Link></li>
             </ul>
+            <div className={styles.leftMenu}>
+                <Link to="/login" onClick={() => disconnection()}>התנתקות</Link>
+            </div>
         </div>
     )
 }
