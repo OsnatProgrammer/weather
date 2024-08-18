0cef72d70ffeb0df1b4edc7b0d2607a0bd7b83a0
osnat <osnat00972@gmail.com>
2024-08-18 15:58:43 +0300

    use function handleSearch

--- src/component/search/search.js
+++ src/component/search/search.js
@@ -22,7 +22,7 @@ export default function Search(props) {
                 })}
             </select>
 
-            <button onClick={() => { props.SetCurrentCity(selectRef.current.value) }}>
+            <button onClick={() => { handleSearch(selectRef.current.value) }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search">
                     <circle cx="11" cy="11" r="8"></circle>
                     <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
